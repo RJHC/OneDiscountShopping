8affa3aa053c9ab01653589405bb8c36f1b3027b
RJHC <812567634@qq.com>
2016-07-25 11:34:27 +0800

    7/25 jhc 第三次分享  有关部分的英文写法

--- app/src/main/java/com/example/onediscountshopping/MainActivity.java
+++ app/src/main/java/com/example/onediscountshopping/MainActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 
 /**
  * 首页部分
+ * 分类搜索 Category Search
+ * 品牌特卖 Brand Sale
+ * 值得逛  Worth around
+ * 个人中心  My Center
  */
 public class MainActivity extends AppCompatActivity {
 
